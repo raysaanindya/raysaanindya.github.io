796e54989cadee3b5b4b6937557931d2a609baab
raysaanindya <raysa.anindya81@smk.belajar.id>
2023-10-04 08:00:19 -0700

    first commit

---
+++ bootstrap/array.js
@@ -0,0 +1,15 @@
+// const siswa = ["Ahmad","Budi","Citra","Desi","Edi"];
+// siswa.push("fikri")
+// console.log(siswa[2]); 
+
+// const buah = ["Mangga","Jeruk","Apel","Melon"];
+// buah.push("Pepaya")
+// buah[1] = "pir"
+// console.table(buah);
+
+const pegawai = [
+    [123,"Ahmad","L"],
+    [124,"Budi","L"],
+    [125,"Citra","p"],
+    [126,"Desi",]
+]
---
+++ bootstrap/for.js
@@ -0,0 +1,8 @@
+for(const i=1; i<=10; i++){
+    console.log('Hallo ke-${i}');
+}
+let x = 0;
+while(x <= 10){
+    console.log('Halo while ke-${x}')
+    x++;
+}
